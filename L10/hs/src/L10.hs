4b599bc055a69150f46908f72f272f7bf200eb20
durhenkd <gavrilatiberiu26@gmail.com>
2021-12-10 15:25:12 +0000

    L010 done

--- L10/hs/src/L10.hs
+++ L10/hs/src/L10.hs
@@ -2,6 +2,22 @@ module L01 where
 
 import Container
 import YesNo
+import RevSort
+
+{-
+    Q 10.8.1
+    > data Stuff = Stuff
+    > *
+
+    > data Option a = None | Some a
+    > * -> * 
+
+    >
+    >
+
+    >
+    >
+-}
 
 {- E 10.9.1. Given the following data definition: data Op = Add | Sub | Mul | Div , implement Show
 such that the corresponding mathematical operator is shown for each instance (i.e. + for
@@ -16,24 +32,39 @@ instance Show Op where
 
 {- E 10.9.2 Implement the YesNo type class for the Maybe type.-}
 
-instance YesNo Maybe where
+instance YesNo (Maybe a) where
     yesno Nothing = False
     yesno _ = True
 
-{- E 10.10.2 Implement the Container type class for a binary tree data type, which is defined as:
+{- E 10.9.3 Implement the Container type class for a binary tree data type, which is defined as:
 data Tree a = Nil | Node (Tree a) a (Tree a) .-}
 
 data Tree a = Nil | Node (Tree a) a (Tree a)
 
+
 instance Container Tree where
-    hasElem _ Nil = False
     hasElem Nil _ = False
     hasElem t e = 
         case t of
             Nil -> False
             Node left us right -> if us == e then True 
-                        else if any (True ==) (hasElem left e) then True
-                        else any (True ==) (hasElem right e)
+                        else (hasElem left e) || (hasElem right e)
 
-        
+    nrElems t =
+        case t of
+            Nil -> 0
+            Node left us right -> 1 + (nrElems right) + (nrElems left)
+    
 
+{- E 10.9.4 Given the newtype Rev , defined as newtype Rev a = Rev a deriving (Eq, Show) im-
+plement Ord such that the sortRev :: (Ord a) => [a] -> [a] function, defined as
+sortRev = sortOn Rev will sort the list in reverse.-}
+
+instance Ord (Rev a) where
+    compare x y = 
+        case x of
+            Rev a ->
+                case y of
+                    Rev b -> if a >= b then LT 
+                        else if a <= b then GT
+                        else EQ 
--- L10/hs/src/RevSort.hs
+++ L10/hs/src/RevSort.hs
@@ -6,5 +6,5 @@ import Data.List as L
 newtype Rev a = Rev a deriving (Show, Eq)
 
 sortRev :: (Ord a) => [a] -> [a]
-sortRev = L.sortOn Rev
+sortRev = L.sort
 
